f51597f1419632b89e93ce86b256b1ddd5322ce7
mikl14 <mikl.eli2002@gmail.com>
2023-02-03 20:13:06 +0300

    Update drone.php

--- drone.php
+++ drone.php
@@ -6,10 +6,19 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
   
-    <script>
-
-
-    </script>
+  <script>
+            var example = document.getElementById("example"),
+          ctx     = example.getContext('2d');
+            
+            ctx.strokeRect(15, 15, 266, 266);
+            ctx.strokeRect(18, 18, 260, 260);
+            ctx.fillRect(20, 20, 256, 256);
+            for (i = 0; i < 8; i += 2)
+                for (j = 0; j < 8; j += 2) {
+                    ctx.clearRect(20 + i * 32, 20 + j * 32, 32, 32);
+                    ctx.clearRect(20 + (i + 1) * 32, 20 + (j + 1) * 32, 32, 32);
+                }
+        </script>
 
  </head>
 
