9b2f8a3328c385067df1c11e344f7233b581381c
mikl14 <mikl.eli2002@gmail.com>
2023-02-03 20:29:26 +0300

    Update drone.php

--- drone.php
+++ drone.php
@@ -13,15 +13,8 @@
          <script>
             var chart = document.getElementById("chart");
            var ctx = chart.getContext('2d');
-            
-            ctx.strokeRect(15, 15, 266, 266);
-            ctx.strokeRect(18, 18, 260, 260);
-            ctx.fillRect(20, 20, 256, 256);
-            for (i = 0; i < 8; i += 2)
-                for (j = 0; j < 8; j += 2) {
-                    ctx.clearRect(20 + i * 32, 20 + j * 32, 32, 32);
-                    ctx.clearRect(20 + (i + 1) * 32, 20 + (j + 1) * 32, 32, 32);
-                }
+            ctx.strokeStyle = 'rgba(255, 255, 255, 1) ';
+           ctx.lineTo(0, 100);
         </script>
 
       </canvas>
