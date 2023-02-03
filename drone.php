20c26e757150a51b2c09b8b390d9b862e23a8e2a
mikl14 <107858531+mikl14@users.noreply.github.com>
2023-02-03 19:45:57 +0300

    Add files via upload

---
+++ drone.php
@@ -0,0 +1,25 @@
+<html>
+ <head>
+  <title>AI</title>
+  <meta charset="utf-8"> 
+  <link rel="stylesheet" href="style.css"  type="text/css"> 
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+  
+    <script>
+
+
+    </script>
+
+ </head>
+
+ <body>
+    <div id = "main">
+
+    </div>
+
+    <div id = "controls">
+
+    </div>
+ </body>
+</html>
